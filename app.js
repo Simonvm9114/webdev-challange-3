2d08eb1f4222683c8eea22c20d3183c232983431
Simonvm91 <simon.van.meegdenburg@climber.nl>
2020-09-20 19:07:29 +0200

    Completed up to challange 6

--- app.js
+++ app.js
@@ -15,7 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+  res.render('home', { startingContent: homeStartingContent })
+})
 
+app.get('/about', (req, res) => {
+  res.render('about', { startingContent: aboutContent })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { startingContent: contactContent })
+})
 
 
 
