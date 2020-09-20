828c4df2aead5b217220991148a96e8441acbc6f
Simonvm91 <simon.van.meegdenburg@climber.nl>
2020-09-20 20:38:31 +0200

    Completed up to challange 15

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// website settings 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// global variables
+let posts = [] 
+
+// website routes
 app.get('/', (req, res) => {
-  res.render('home', { startingContent: homeStartingContent })
+  res.render('home', { startingContent: homeStartingContent, blogPosts: posts })
 })
 
 app.get('/about', (req, res) => {
@@ -37,7 +41,9 @@ app.post('/compose', (req, res) => {
     Body: req.body.postContent
   }
 
-  console.log(post)
+  posts.push(post)
+  res.redirect('/')
+
 })
 
 
